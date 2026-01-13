067f7d40c66f6e35eb092fa433f615aa410de034
StamatisChr <stamatios.chrysinas@hashicorp.com>
2026-01-13 10:32:12 +0100

    add required providers block and 2 variables and a variable set

--- main.tf
+++ main.tf
@@ -1,12 +1,21 @@
 # Terraform code to create resources in TFE (org and workspace)
 
+terraform {
+  required_providers {
+    tfe = {
+      source  = "hashicorp/tfe"
+      version = "~> 0.73.0"
+    }
+  }
+}
+
 provider "tfe" {
   hostname = var.tfe_hostname
   token    = var.admin_token
 }
 
 resource "tfe_organization" "test-organization" {
-  name  = "${var.org_name}"
+  name  = var.org_name
   email = var.admin_email
 }
 
@@ -24,6 +33,29 @@ resource "tfe_workspace" "test" {
     admin-api-token = var.admin_api_token
   }
 }
+
+resource "tfe_variable" "test" {
+  key          = "mycount"
+  value        = "1"
+  category     = "terraform"
+  description  = "A useful description for the test variable"
+  workspace_id = tfe_workspace.test.id
+}
+
+resource "tfe_variable" "test2" {
+  key             = "my_var_name"
+  value           = "my_var_value"
+  category        = "terraform"
+  description     = "A useful description for the test variable"
+  variable_set_id = tfe_variable_set.test.id
+}
+
+resource "tfe_variable_set" "test" {
+  name         = "Test Variable set"
+  description  = "Description for test variable set"
+  organization = tfe_organization.test-organization.name
+}
+
 
 resource "tfe_oauth_client" "github" {
   organization = tfe_organization.test-organization.name
--- variables.tf
+++ variables.tf
@@ -1,18 +1,20 @@
-variable "oauth_token" {}
+variable "oauth_token" {
+  default = ""
+}
 variable "admin_token" {}
 variable "admin_api_token" {}
 variable "tfe_hostname" {}
 variable "org_name" {
-    default = "my-organization"
+  default = "my-organization"
 }
 variable "workspace_name" {
-    default = "my-workspace"
+  default = "my-workspace"
 }
 variable "admin_email" {
-    default = "example@example.com"
+  default = "example@example.com"
 }
 variable "github_repo" {
-    default ="TonyPeponi/terraform_null_resources"
+  default = "TonyPeponi/terraform_null_resources"
 }
 
 
